770550a6523af67f41b0899a67801d03ac3c53e1
Sheikh-hadi <sheikhgee322@gmail.com>
2024-08-05 15:57:10 +0500

    ++

--- src/CommonComponent/Accordian/Accordian.js
+++ src/CommonComponent/Accordian/Accordian.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Collapse, Space } from 'antd';
 import './Accordian.css';
 
@@ -6,6 +6,16 @@ const { Panel } = Collapse;
 
 const Accordian = (props) => {
     const { accordianModelLeft, accordianModelRight } = props.accordian;
+    const [activeKeyLeft, setActiveKeyLeft] = useState([]);
+    const [activeKeyRight, setActiveKeyRight] = useState([]);
+
+    const onChangeLeft = (key) => {
+        setActiveKeyLeft(key);
+    };
+
+    const onChangeRight = (key) => {
+        setActiveKeyRight(key);
+    };
 
     return (
         <div className='mt-4 main'>
@@ -13,19 +23,37 @@ const Accordian = (props) => {
                 <div className='col-xs-12 col-sm-12 col-md-6 col-lg-6'>
                     <Space direction="vertical" style={{ width: '100%' }}>
                         {accordianModelLeft.map((li, index) => (
-                            <Collapse className="full-width-collapse" key={`left-${index}`}>
-                                <Panel header={li.title} key={index}>
+                            <Collapse
+                                className="full-width-collapse"
+                                key={`left-${index}`}
+                                activeKey={activeKeyLeft}
+                                onChange={onChangeLeft}
+                            >
+                                <Panel
+                                    header={li.title}
+                                    key={index}
+                                    className={activeKeyLeft.includes(index.toString()) ? 'open-panel-header' : ''}
+                                >
                                     <p>{li.text}</p>
                                 </Panel>
                             </Collapse>
                         ))}
                     </Space>
                 </div>
-                <div className='col-xs-12 col-sm-12 col-md-6 col-lg-6 mb-4'>
+                <div className='col-xs-12 col-sm-12 col-md-6 col-lg-6'>
                     <Space direction="vertical" style={{ width: '100%' }}>
                         {accordianModelRight.map((li, index) => (
-                            <Collapse className="full-width-collapse" key={`right-${index}`}>
-                                <Panel header={li.title} key={index}>
+                            <Collapse
+                                className="full-width-collapse"
+                                key={`right-${index}`}
+                                activeKey={activeKeyRight}
+                                onChange={onChangeRight}
+                            >
+                                <Panel
+                                    header={li.title}
+                                    key={index}
+                                    className={activeKeyRight.includes(index.toString()) ? 'open-panel-header' : ''}
+                                >
                                     <p>{li.text}</p>
                                 </Panel>
                             </Collapse>
