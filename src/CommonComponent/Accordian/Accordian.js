29f0c0bf472e083233fda3df12aa4ef798106492
Sheikh-hadi <sheikhgee322@gmail.com>
2024-08-05 15:52:30 +0500

    ++

--- src/CommonComponent/Accordian/Accordian.js
+++ src/CommonComponent/Accordian/Accordian.js
@@ -5,27 +5,26 @@ import './Accordian.css';
 const { Panel } = Collapse;
 
 const Accordian = (props) => {
-    let AccordianModelLeft = props.accordian.accordianModelLeft;
-    let accordianModelRight = props.accordian.accordianModelRight;
-    
+    const { accordianModelLeft, accordianModelRight } = props.accordian;
+
     return (
-        <div className='container mt-4'>
-            <div className='row g-3'> 
-                <div className='col-6'>
-                    <Space direction="vertical">
-                        {AccordianModelLeft.map((li, index) => (
-                    <Collapse className="full-width-collapse" >
-                            <Panel header={li.title} key={index}>
-                                <p>{li.text}</p>
-                            </Panel>
-                    </Collapse>
+        <div className='mt-4 main'>
+            <div className='row g-5'>
+                <div className='col-xs-12 col-sm-12 col-md-6 col-lg-6'>
+                    <Space direction="vertical" style={{ width: '100%' }}>
+                        {accordianModelLeft.map((li, index) => (
+                            <Collapse className="full-width-collapse" key={`left-${index}`}>
+                                <Panel header={li.title} key={index}>
+                                    <p>{li.text}</p>
+                                </Panel>
+                            </Collapse>
                         ))}
                     </Space>
                 </div>
-                <div className='col-6'>
-                    <Space direction="vertical">
+                <div className='col-xs-12 col-sm-12 col-md-6 col-lg-6 mb-4'>
+                    <Space direction="vertical" style={{ width: '100%' }}>
                         {accordianModelRight.map((li, index) => (
-                            <Collapse className="full-width-collapse" >
+                            <Collapse className="full-width-collapse" key={`right-${index}`}>
                                 <Panel header={li.title} key={index}>
                                     <p>{li.text}</p>
                                 </Panel>
