13e1e4e632bc06d98ac29be77c23b91f123ae39f
Sheikh-hadi <sheikhgee322@gmail.com>
2024-08-05 12:16:55 +0500

    ++

--- src/Model/Card/Tiktok/buyTiktokFollowersCardModel.js
+++ src/Model/Card/Tiktok/buyTiktokFollowersCardModel.js
@@ -1,40 +1,9 @@
-const buyInstagramAutoReelsPackagesCards = [
+const buyTiktokFollowersCardModel = [
     {
         id: 1,
-        icon: "fas fa-shopping-cart icon",
-        title: "Choose a Package",
-        description: "Select the Instagram-like package that suits your needs.",
-        style: {
-            background: {
-                backgroundColor: '#b981f2',
-            },
-            heading1: {
-                fontSize: "50px",
-                color: "#ffffff42",
-                margin: "0px",
-            },
-            heading2: {
-                fontSize: "20px",
-                color: "#ffffffe6",
-                margin: "0px",
-            },
-            description: {
-                margin: "10px 0",
-                color: "#FFFFFFD4",
-                fontSize: "15px",
-            },
-            icon: {
-                fontSize: "30px",
-                color: "white",
-            }
-
-        }
-    },
-    {
-        id: 2,
         icon: "fas fa-mail-bulk icon",
-        title: "Enter Details",
-        description: " Provide your Instagram post link and contact details.",
+        title: "Real and Active Followers:",
+        description: "We deliver followers who are real users with active accounts",
         style: {
             background: {
                 backgroundColor: '#e3f8ec',
@@ -61,10 +30,10 @@ const buyInstagramAutoReelsPackagesCards = [
         }
     },
     {
-        id: 3,
+        id: 2,
         icon: "fas fa-money-check-alt icon",
-        title: "Make Payment",
-        description: "Pay securely using PayPal or your card. Your likes will start arriving within minutes.",
+        title: "Targeted Audience:",
+        description: "Our followers are from Australia, ensuring that you reach the right audience for your business.",
         style: {
             background: {
                 backgroundColor: '#cdf9fe',
@@ -81,7 +50,67 @@ const buyInstagramAutoReelsPackagesCards = [
             },
             description: {
                 fontSize: "15px",
+                margin: "10 0",
+                color: "#48ac8c",
+            },
+            icon: {
+                fontSize: "30px",
+                color: "plum",
+            }
+        }
+    },
+    {
+        id: 3,
+        icon: "fas fa-mail-bulk icon",
+        title: "Fast Delivery:",
+        description: "You will start seeing an increase in your follower count within a short period of time.",
+        style: {
+            background: {
+                backgroundColor: '#e3f8ec',
+            },
+            heading1: {
+                fontSize: "50px",
+                color: "#9dcca3",
+                margin: "0px",
+            },
+            heading2: {
+                fontSize: "20px",
+                color: "#28c968",
+                margin: "0px",
+            },
+            description: {
                 margin: "10px 0",
+                color: "#5e6d64cc",
+                fontSize: "15px",
+            },
+            icon: {
+                fontSize: "30px",
+                color: "rgb(28, 115, 105)",
+            }
+        }
+    },
+    {
+        id: 4,
+        icon: "fas fa-money-check-alt icon",
+        title: "24/7 Customer Support",
+        description: "24/7 Customer Support: Our dedicated support team is available to assist you with any queries or concerns.",
+        style: {
+            background: {
+                backgroundColor: '#cdf9fe',
+            },
+            heading1: {
+                fontSize: "50px",
+                color: "#48ac8c",
+                margin: "0px",
+            },
+            heading2: {
+                fontSize: "20px",
+                margin: "0",
+                color: "#449099",
+            },
+            description: {
+                fontSize: "15px",
+                margin: "10 0",
                 color: "#48ac8c",
             },
             icon: {
@@ -91,4 +120,4 @@ const buyInstagramAutoReelsPackagesCards = [
         }
     },
 ];
-export default buyInstagramAutoReelsPackagesCards;
+export default buyTiktokFollowersCardModel;
---
+++ src/Model/Card/instagramFollowersCardModel.js
@@ -0,0 +1,123 @@
+const instagramFollowersCardModel = [
+    {
+        id: 1,
+        icon: "fas fa-mail-bulk icon",
+        title: "Instant credibility",
+        description: "More followers make it easier to get noticed. Buying Instagram followers boosts your profile's reputation, making your account look famous.",
+        style: {
+            background: {
+                backgroundColor: '#e3f8ec',
+            },
+            heading1: {
+                fontSize: "50px",
+                color: "#9dcca3",
+                margin: "0px",
+            },
+            heading2: {
+                fontSize: "20px",
+                color: "#28c968",
+                margin: "0px",
+            },
+            description: {
+                margin: "10px 0",
+                color: "#5e6d64cc",
+                fontSize: "15px",
+            },
+            icon: {
+                fontSize: "30px",
+                color: "rgb(28, 115, 105)",
+            }
+        }
+    },
+    {
+        id: 2,
+        icon: "fas fa-money-check-alt icon",
+        title: "Save time and work",
+        description: "Growing your Instagram takes time. Buy followers to save time, create better content, and attract real followers.",
+        style: {
+            background: {
+                backgroundColor: '#cdf9fe',
+            },
+            heading1: {
+                fontSize: "50px",
+                color: "#48ac8c",
+                margin: "0px",
+            },
+            heading2: {
+                fontSize: "20px",
+                margin: "0",
+                color: "#449099",
+            },
+            description: {
+                fontSize: "15px",
+                margin: "10 0",
+                color: "#48ac8c",
+            },
+            icon: {
+                fontSize: "30px",
+                color: "plum",
+            }
+        }
+    },
+    {
+        id: 3,
+        icon: "fas fa-mail-bulk icon",
+        title: "Help your business grow.",
+        description: "More Instagram followers boost your business's credibility, attracting customers, increasing sales, and improving your brand's reputation.P",
+        style: {
+            background: {
+                backgroundColor: '#e3f8ec',
+            },
+            heading1: {
+                fontSize: "50px",
+                color: "#9dcca3",
+                margin: "0px",
+            },
+            heading2: {
+                fontSize: "20px",
+                color: "#28c968",
+                margin: "0px",
+            },
+            description: {
+                margin: "10px 0",
+                color: "#5e6d64cc",
+                fontSize: "15px",
+            },
+            icon: {
+                fontSize: "30px",
+                color: "rgb(28, 115, 105)",
+            }
+        }
+    },
+    {
+        id: 4,
+        icon: "fas fa-money-check-alt icon",
+        title: "Improves Engagement",
+        description: "Improves engagement by increasing likes, comments, and shares on your posts. More followers mean more people.",
+        style: {
+            background: {
+                backgroundColor: '#cdf9fe',
+            },
+            heading1: {
+                fontSize: "50px",
+                color: "#48ac8c",
+                margin: "0px",
+            },
+            heading2: {
+                fontSize: "20px",
+                margin: "0",
+                color: "#449099",
+            },
+            description: {
+                fontSize: "15px",
+                margin: "10 0",
+                color: "#48ac8c",
+            },
+            icon: {
+                fontSize: "30px",
+                color: "plum",
+            }
+        }
+    },
+];
+export default instagramFollowersCardModel;
