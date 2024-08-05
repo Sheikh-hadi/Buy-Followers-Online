d7b851f8f0e37f4e22eef469ab80b88360022e65
Sadia7725 <sadiasafdar7725@gmail.com>
2024-08-05 17:30:08 +0500

    ++

---
+++ src/Compoents/Block/Block.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Block.css";
+
+export default function Block() {
+  return (
+    <div>
+      <div className="block py-5">
+        <div className="inner_block d-flex flex-column flex-md-row justify-content-center align-items-center">
+          <img src="./block.png" alt="" className="img1 mb-4 mb-md-0" />
+          <p className="inner_block_para">Boost your Instagram Presence with our award-winning services.</p>
+        </div>
+      </div>
+
+      
+    </div>
+  );
+}
--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -4,6 +4,8 @@ import Accordian from '../CommonComponent/Accordian/Accordian';
 import { accordianModelLeft, accordianModelRight } from '../Model/AccordianModel/aboutAccordianModel'
 import AboutUsIntro from '../Compoents/AboutUsIntro/AboutUsIntro';
 import Cards from '../Compoents/Cards/Cards';
+import Block from '../Compoents/Block/Block';
+import CenteredTextSection from '../Compoents/InfoSection/centeredTextSection';
 
 const AboutUs = () => {
   console.log("accordianModelLeft: ", accordianModelLeft, "accordianModelRight: ", accordianModelRight)
@@ -11,6 +13,9 @@ const AboutUs = () => {
     <>
     <AboutUsIntro/>
     <Cards/>
+    <Block/>
+    <CenteredTextSection/>
+   
       <Accordian accordian={{accordianModelLeft, accordianModelRight}} />
     </>
   )
