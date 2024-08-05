d7a7d946d6a00756a17fdc36ef7553f33ab48636
Sadia7725 <sadiasafdar7725@gmail.com>
2024-08-05 15:57:52 +0500

    ++

---
+++ src/Compoents/Cards/Cards.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Cards.css";
+
+export default function Cards() {
+  return (
+    <div className="custom-container py-5">
+      <div className="row card_about justify-content-center">
+        <div className="col-12 col-md-6 col-lg-4 mb-4">
+          <div className="card p-4 h-100">
+            <i className="fas fa-cog mb-3"></i>
+            <h3 className="card_heading">Authentic Services</h3>
+            <p className="card_paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-lg-4 mb-4">
+          <div className="card p-4 h-100">
+            <i className="fas fa-hand-pointer mb-3"></i>
+            <h3 className="card_heading">Easy To Use</h3>
+            <p className="card_paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-lg-4 mb-4">
+          <div className="card p-4 h-100">
+            <i className="fas fa-lock mb-3"></i>
+            <h3 className="card_heading">Secure Payment</h3>
+            <p className="card_paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Accordian from '../CommonComponent/Accordian/Accordian';
 
 import { accordianModelLeft, accordianModelRight } from '../Model/AccordianModel/aboutAccordianModel'
+import AboutUsIntro from '../Compoents/AboutUsIntro/AboutUsIntro';
+import Cards from '../Compoents/Cards/Cards';
 
 const AboutUs = () => {
   console.log("accordianModelLeft: ", accordianModelLeft, "accordianModelRight: ", accordianModelRight)
   return (
     <>
+    <AboutUsIntro/>
+    <Cards/>
       <Accordian accordian={{accordianModelLeft, accordianModelRight}} />
     </>
   )
