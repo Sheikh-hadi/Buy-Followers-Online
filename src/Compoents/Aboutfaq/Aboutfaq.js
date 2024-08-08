d15541114b2b532685191836094a8420a9e92d4a
Sadia7725 <sadiasafdar7725@gmail.com>
2024-08-08 15:16:42 +0500

    ++

---
+++ src/Compoents/Aboutfaq/Aboutfaq.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Aboutfaq.css'
+const Aboutfaq = () => {
+  return (
+    <div className="fq-section">
+    <h2>FAQs</h2>
+    <p>Frequently Asked Questions</p>
+  </div>
+
+  )
+}
+
+export default Aboutfaq
