b5588726d0fb4bc1e6a373f0dfc925fa5bbaf918
Sheikh-hadi <sheikhgee322@gmail.com>
2024-08-09 11:57:42 +0500

    ++

--- src/CommonComponent/ImageContentBlock/ImageContentBlock.js
+++ src/CommonComponent/ImageContentBlock/ImageContentBlock.js
@@ -7,28 +7,28 @@ const ImageContentBlock = (props) => {
 
     return (
         <div className="container mt-4 mb-4">
-            <div className='row' style={{border:"2px solid black"}} >
+            <div className='row'  >
                 {blockList.map((block, index) => {
 
                     return (
-                        <div key={index} className='row  mb-3' style={{ border: "2px solid black" }}>
+                        <div key={index} className='row  mb-3' >
                             {(block.id % 2 === 0) ?
                                 <>
-                                    <div className='col-4 img' style={{ border: "2px solid black" }}>
+                                    <div className='col-4 img' >
                                         <img src={block.image} alt='Packages at Low Prices' style={{height: "200px", alignContent: "center" }} />
                                     </div>
-                                    <div className='col-8' style={{ border: "2px solid black" }}>
+                                    <div className='col-8' >
                                         <h1 className='heading'>{block.title}</h1>
                                         <p className='para'>{block.description}</p>
                                     </div>
                                 </>
                                 :
                                 <>
-                                    <div className='col-8' style={{ border: "2px solid black" }}>
+                                    <div className='col-8' >
                                         <h1 className='heading'>{block.title}</h1>
                                         <p className='para'>{block.description}</p>
                                     </div>
-                                    <div className='col-4' style={{ border: "2px solid black" }}>
+                                    <div className='col-4' >
                                         <img src={block.image} alt='Packages at Low Prices' style={{height: "200px", alignContent: "center" }} />
                                     </div>
                                 </>
