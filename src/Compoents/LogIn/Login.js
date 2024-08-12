41185f16bdd02ec1c478d995f34384f380c0cc04
Sadia7725 <sadiasafdar7725@gmail.com>
2024-08-12 18:00:50 +0500

    ++

--- src/Compoents/LogIn/Login.js
+++ src/Compoents/LogIn/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Button, Form } from 'react-bootstrap';
-import './Login.css';  // Ensure you have this stylesheet for the custom styling.
+import './Login.css';
 
 const Login = () => {
   const [active, setActive] = useState(false);
@@ -8,27 +8,50 @@ const Login = () => {
   return (
     <Container fluid className={`d-flex justify-content-center align-items-center login-container ${active ? 'active' : ''}`}>
       <Row className="container-box">
-        <Col md={6} className="form-container sign-in">
+        <Col md={6} className={`form-container sign-up ${active ? 'active' : ''}`}>
+          <Form>
+            <h1>Create Account</h1>
+            <div className="social-icons">
+              <a href="/" className="icon"><i className="fab fa-google-plus-g"></i></a>
+              <a href="/" className="icon"><i className="fab fa-facebook-f"></i></a>
+              <a href="/" className="icon"><i className="fab fa-github"></i></a>
+              <a href="/" className="icon"><i className="fab fa-linkedin-in"></i></a>
+            </div>
+            <span>or use your email for registration</span>
+            <Form.Control type="text" placeholder="Name" />
+            <Form.Control type="email" placeholder="Email" />
+            <Form.Control type="password" placeholder="Password" />
+            <Button className="mt-3 btn-signup">Sign Up</Button>
+          </Form>
+        </Col>
+        <Col md={6} className={`form-container sign-in ${active ? '' : 'active'}`}>
           <Form>
             <h1>Sign In</h1>
             <div className="social-icons">
               <a href="/" className="icon"><i className="fab fa-google-plus-g"></i></a>
               <a href="/" className="icon"><i className="fab fa-facebook-f"></i></a>
-              <a href="/" className="icon"><i class="fa-brands fa-instagram"></i></a>
-              <a href="/" className="icon"><i class="fa-brands fa-tiktok"></i></a>
+              <a href="/" className="icon"><i className="fab fa-github"></i></a>
+              <a href="/" className="icon"><i className="fab fa-linkedin-in"></i></a>
             </div>
-            <span>or use your email password</span>
+            <span>or use your email for registration</span>
             <Form.Control type="email" placeholder="Email" />
             <Form.Control type="password" placeholder="Password" />
             <a href="/" className="forgot-password">Forget Your Password?</a>
             <Button className="mt-3 btn-signin">Sign In</Button>
           </Form>
         </Col>
-        <Col md={6} className="d-flex align-items-center justify-content-center toggle-panel toggle-right">
-          <div>
-            <h1>Hello, Friend!</h1>
-            <p>Register with your personal details to use all of the site features.</p>
-            <Button onClick={() => setActive(true)} className="btn-signup">Sign Up</Button>
+        <Col md={6} className="toggle-container">
+          <div className={`toggle ${active ? 'active' : ''}`}>
+            <div className="toggle-panel toggle-left">
+              <h1>Welcome Back!</h1>
+              <p className='Persnal-detail'>Enter your personal details to use all of the site features.</p>
+              <Button onClick={() => setActive(false)} className="btn-signin">Sign In</Button>
+            </div>
+            <div className="toggle-panel toggle-right">
+              <h1>Hello, Friend!</h1>
+              <p className='Persnal-detail'>Register with your personal details to use all of the site features.</p>
+              <Button onClick={() => setActive(true)} className="btn-signup">Sign Up</Button>
+            </div>
           </div>
         </Col>
       </Row>
