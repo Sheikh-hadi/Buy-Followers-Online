586e729ad65aa3be4e39289bb2d93a9890238f12
Sadia7725 <sadiasafdar7725@gmail.com>
2024-08-23 12:19:13 +0500

    ++

--- src/Compoents/HomeComponent/Feature/Features.js
+++ src/Compoents/HomeComponent/Feature/Features.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import './Features.css';
+import React from "react";
+import "./Features.css";
 
-const Features = ({features}) => {
-
-
+const Features = ({ features }) => {
   return (
     <div className="container features-container">
-      <div className="row">
+      <div
+        className="row"
+        style={{ display: "flex", flexwrap: "wrap", justifycontent: "center" }}
+      >
         {features.map((feature, index) => (
           <div key={index} className="col-lg-6 col-md-6 col-12 mb-4">
-            <div className="feature-item">
+            <div className="feature-item"  style={{ display: "flex", flexwrap: "wrap", justifycontent: "center" }}>
               <span className="feature-number">{feature.number}</span>
               <div>
                 <h4 className="feature-title">{feature.title}</h4>
